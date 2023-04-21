202ff8d3907489da0d71d164e9d6a02c0afb0b24
GokilaVani-Maibalan <gokilamaibalan64@gmail.com>
2023-04-21 21:59:30 +0530

    "change-new"

---
+++ src/components/Login.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+import { url } from "../App";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let navigate = useNavigate();
+
+  // to clear session storage
+  useEffect(() => {
+    sessionStorage.clear();
+  }, []);
+
+  let handleLogin = async () => {
+    let payload = { email, password };
+
+    try {
+      let res = await axios.post(`${url}/mentor/login`, payload);
+      toast.success(res.data.message);
+      //to store in session storage
+      sessionStorage.setItem("token", res.data.token);
+      //after login and storing navigate to dashboard
+      navigate("/dashboard");
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+      // toast.error(error.response.data.message)
+      toast.error(error.response.data.message);
+    }
+  };
+  return (
+    <div className="login-wrapper">
+      <h1 style={{ textAlign: "center" }}>Login Here</h1>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" onClick={() => handleLogin()}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Login;
